52494415817c45f7571d56008d280d0d4ba2c5c2
ClementKwesiBaffourPalmer <clementbaffour@Drs-MacBook-Pro.local>
2023-11-03 07:18:19 +0000

    Note_taking app implementation

---
+++ document/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Document(models.Model):
+    title = models.CharField(max_length=225)
+    content = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ('title',)
